f6b899f343a9b7cfb3047b0022d07efd9f7b56c2
Sathya Ram <sathyatheram@gmail.com>
2019-04-25 21:50:17 -0400

    visual changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './Globals.scss';
-import Header from './Header/Header';
-import Main from './Main/Main';
+import "./Globals.scss";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      color: '',
-      name: 'name',
-      url: 'url',
-      image: 'image'
+      color: "",
+      name: "name",
+      url: "url",
+      image: "image"
     };
   }
+
   render() {
     return (
       <div className="page">
