b12c0dff224c13226426fd9baeb6021453e58a87
Sathya Ram <sathyatheram@gmail.com>
2019-06-20 00:20:10 -0400

    Added build

---
+++ build/precache-manifest.3a3de60ea63dab5466ea94a0098e370a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0607f477c9c3af169a73882c7f1f9259",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e5de4cdd676040b12a7",
+    "url": "/static/css/main.34b2cbe5.chunk.css"
+  },
+  {
+    "revision": "742a5ce72f0361f1979f",
+    "url": "/static/js/2.a8475f3e.chunk.js"
+  },
+  {
+    "revision": "5e5de4cdd676040b12a7",
+    "url": "/static/js/main.b664ff56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
