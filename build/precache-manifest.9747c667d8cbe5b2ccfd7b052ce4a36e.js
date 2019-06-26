3408ebeb05e3491875ef99812cf45708992bda77
Sathya Ram <sathyatheram@gmail.com>
2019-06-25 22:57:16 -0400

    Build for textcolor

--- build/precache-manifest.9747c667d8cbe5b2ccfd7b052ce4a36e.js
+++ build/precache-manifest.9747c667d8cbe5b2ccfd7b052ce4a36e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d46f34c8769b967b9d98837ef4c62162",
+    "revision": "c3db96471e0439b16cd4d0cd8d4b3e90",
     "url": "/index.html"
   },
   {
-    "revision": "619c4eb782dbadaf8eba",
-    "url": "/static/css/main.0ea40f23.chunk.css"
+    "revision": "04b8360be868a44fb6b3",
+    "url": "/static/css/main.3bb5bf52.chunk.css"
   },
   {
     "revision": "742a5ce72f0361f1979f",
     "url": "/static/js/2.a8475f3e.chunk.js"
   },
   {
-    "revision": "619c4eb782dbadaf8eba",
-    "url": "/static/js/main.515a281f.chunk.js"
+    "revision": "04b8360be868a44fb6b3",
+    "url": "/static/js/main.ebb21725.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82a1ff965bb6376341a746c6b6c971dd.js"
+  "/precache-manifest.9747c667d8cbe5b2ccfd7b052ce4a36e.js"
 );
 
 self.addEventListener('message', (event) => {
